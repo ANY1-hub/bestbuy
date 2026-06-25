271f21c0fd868ab389f1398c2344ad80f6db529c
nyiry <dev@bejelentkezesek.mozmail.com>
2026-06-25 17:29:42 +0200

    Class documentation

--- products.py
+++ products.py
@@ -1,4 +1,7 @@
 class Product():
+    """ Represents a specific type of product available in the store (For example, MacBook Air M2).
+        It encapsulates information about the product, including its name and price. It includes an
+        attribute to keep track of the total quantity of items of that product currently available in the store."""
     def __init__(self, name, price, quantity, active=True):
         if name == '':
             raise ValueError("Sorry, name is not allowed to be empty")
@@ -15,62 +18,62 @@ class Product():
         self.active = active
 
     def get_quantity(self) -> int:
-        '''
+        """
         Getter function for quantity.
         :return: quantity (int)
-        '''
+        """
         return self.quantity
 
     def set_quantity(self, quantity):
-        '''
+        """
         Setter function for quantity. If quantity reaches -1, deactivates the product.
         :param quantity:
         :return: True if the product is active, otherwise False.
-        '''
+        """
         if quantity <= -1:
             self.active = False
         self.quantity = quantity
 
     def is_active(self) -> bool:
-        '''
+        """
         Getter function for active.
         :return:
-        '''
+        """
         return self.active
 
 
     def activate(self):
-        '''
+        """
         Activates the product.
-        '''
+        """
         self.active = True
 
 
     def deactivate(self):
-        '''
+        """
         Deactivates the product
         :return:
-        '''
+        """
         self.active = False
 
 
     def show(self):
-        '''
+        """
         Prints a string that represents the product, for example:
         "MacBook Air M1, Price: 1450, Quantity: 100"
         :return:
-        '''
+        """
         print(f'{self.name}, Price: {self.price }, Quantity: {self.quantity}')
 
 
     def buy(self, quantity) -> float:
-        '''
+        """
         Buys a given quantity of the product.
         Updates the quantity of the product.
         In case of a problem, raises an Exception.
         :param quantity:
         :return: total price (float) of the purchase
-        '''
+        """
         if quantity < 0:
             raise ValueError(f'Sorry, you cannot buy negative quantities.')
         if self.quantity - quantity < 0:
--- store.py
+++ store.py
@@ -4,7 +4,7 @@ from products import Product
 
 
 class Store():
-
+    """contains one variable - a list of products that exist in the store and facilitates multi product purchase"""
     def __init__(self, products: list):
         if not isinstance(products, list):
             raise ValueError('Sorry, this is not a list form')
@@ -12,26 +12,26 @@ class Store():
 
 
     def add_product(self, product):
-        '''
+        """
         Adds product to the Store(list of self.products)
         :param product: product
-        '''
+        """
         self.products.append(product)
 
 
     def remove_product(self, product):
-        '''
+        """
         Removes a product from store.
         :param product: product
-        '''
+        """
         self.products.remove(product)
 
 
     def get_total_quantity(self) -> int:
-        '''
+        """
         Returns how many items are in the store in total
         :return: items in store total
-        '''
+        """
         number_of_products = 0
         for product in self.products:
             number_of_products += product.get_quantity()
@@ -39,10 +39,10 @@ class Store():
 
 
     def get_all_products(self) -> list[Product]:
-        '''
+        """
         Returns all products in the store that are active
         :return: list of ACTIVE products
-        '''
+        """
         active_products = []
         for product in self.products:
             if product.is_active():
@@ -50,13 +50,13 @@ class Store():
         return active_products
 
     def order(self, shopping_list: list[tuple[Product, int]]) -> float:
-        '''
+        """
         Gets a list of tuples, where each tuple has 2 items:
         Product (Product class) and quantity (int).
         Buys the products and returns the total price of the order.
         :param shopping_list:
         :return: total price of the orde
-        '''
+        """
         shopping_list_total_price = 0
         for product, quantity in shopping_list:
              shopping_list_total_price += product.buy(quantity)
